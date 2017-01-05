030d6033059b9c87d78fdbb099680a7f5b9949a4
remberluyckx <remberluyckx@gmail.com>
2017-01-05 12:32:39 +0100

    implemented navigation drawer + added xml files from other project

--- aRSimpleInteraction/build/generated/source/r/debug/org/artoolkit/ar/base/R.java
+++ aRSimpleInteraction/build/generated/source/r/debug/org/artoolkit/ar/base/R.java
@@ -14,20 +14,20 @@ public final class R {
 		public static final int pref_entryValues_cameraResolution = 0x7f0b0003;
 	}
 	public static final class drawable {
-		public static final int settings = 0x7f020046;
+		public static final int settings = 0x7f020048;
 	}
 	public static final class id {
-		public static final int settings = 0x7f0d0061;
+		public static final int settings = 0x7f0d006f;
 	}
 	public static final class menu {
-		public static final int options = 0x7f0e0000;
+		public static final int options = 0x7f0e0002;
 	}
 	public static final class string {
-		public static final int menuItem_settings = 0x7f060016;
-		public static final int prefCategory_title_camera = 0x7f060017;
-		public static final int pref_defaultValue_cameraResolution = 0x7f060018;
-		public static final int pref_title_cameraIndex = 0x7f060019;
-		public static final int pref_title_cameraResolution = 0x7f06001a;
+		public static final int menuItem_settings = 0x7f060019;
+		public static final int prefCategory_title_camera = 0x7f06001c;
+		public static final int pref_defaultValue_cameraResolution = 0x7f06001d;
+		public static final int pref_title_cameraIndex = 0x7f06001e;
+		public static final int pref_title_cameraResolution = 0x7f06001f;
 	}
 	public static final class xml {
 		public static final int preferences = 0x7f050000;
